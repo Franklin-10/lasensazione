490102203057e090a983049587fbe33d95dce3ce
Franklin <franklin.oliveira096@gmail.com>
2025-04-21 18:56:14 -0300

    alteracao  api url

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,4 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL || '';
+export const API_URL = 'https://palegreen-ibex-350332.hostingersite.com/json';
 
 export function TOKEN_POST(body) {
   return {
